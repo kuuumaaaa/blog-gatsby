d884a77bba3a7ff56e27b9db321078d9c51bbccd
kuuumaaaa <gutodog0520@gmail.com>
2021-10-16 14:52:38 +0900

    add tailwild!!!!!

---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Test = () => {
+  return (
+    <div>
+      <h1 class="text-xl text-red-500">Welcome to Tailwind CSS!</h1>
+    </div>
+  )
+}
+export default Test
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
+// tailwind.config.js
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  // darkMode: false, // or 'media' or 'class'
-  darkMode: media,
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
