04592814d73dc43722672a8bce20d1b8fd8d00b3
kuuumaaaa <gutodog0520@gmail.com>
2021-10-17 10:14:54 +0900

    add contact page

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,6 @@ import { Link } from "gatsby";
 
 const Header = ({ title }) => {
   return (
-    //  <nav>
-    //    <div class = "flex">
-    //       <div class = "w-2/3">
-    //         <Link to="/">
-    //           <ul class="w-64 text-gray-700 text-center py-2 m-2">
-    //           {title}
-    //           </ul>
-    //         </Link>
-    //       </div>
-    //       <div class = "w-1/3">
-    //         <ul class="flex flex-row-reverse space-x-5 fixed">
-    //           <Link to="/">
-    //             <li class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-    //               HOME
-    //             </li>
-    //             </Link>
-    //             <Link to="/about">
-    //             <li class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-    //               ABOUT
-    //             </li>
-    //           </Link>
-    //         </ul>
-    //       </div>
-    //    </div>
-    //  </nav>
     <header class="text-gray-600 body-font bg-yellow-600">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
@@ -39,25 +14,25 @@ const Header = ({ title }) => {
         <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <a class="mr-5 hover:text-gray-900">
             <Link to="/">
-                HOME
+                Home
             </Link>
           </a>
           <a class="mr-5 hover:text-gray-900">
             <Link to="/about">
-                  ABOUT
+                  About
+            </Link>
+          </a>
+          <a class="mr-5 hover:text-gray-900">
+            <Link to="/contact">
+                  Contact
             </Link>
           </a>
           {/* <a class="mr-5 hover:text-gray-900">Third Link</a>
           <a class="mr-5 hover:text-gray-900">Fourth Link</a> */}
         </nav>
-        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Contact
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
       </div>
     </header>
-   );
-}; 
+   )
+}
 
-export default Header;
+export default Header
---
+++ src/pages/contact.js
@@ -0,0 +1,84 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContuctPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <div class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <SEO title="Contuct me" />
+        <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white my-2" >Contuct me</h1>        
+        <form
+          name="contact" 
+          method="POST" 
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field" />
+          <div class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            name
+          </div>
+          <div className="">
+            <input size="40"
+                   type="text"
+                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                   id="name" name="name"
+                   maxlength="30"
+                   minlength="2"
+                   required
+                   autocomplete="name" />
+          </div>
+          <p/>
+          <div class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            e-mail
+          </div>
+          <div className="form-group">
+            <input size="40"
+                   type="email"
+                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                   id="email"
+                   name="email"
+                   pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+                   required
+                   autocomplete="email" />
+          </div>
+          <p/>
+          <div class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            message
+          </div>
+          <div className="form-group">
+            <textarea cols="41"
+                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="contact"
+                      name="content"
+                      rows="8"
+                      required/>
+          </div>
+          <p/>
+          <div class="md:flex md:items-center">
+            <div class="md:w-1/3">
+              <button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 rounded" type="submit">send</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  )
+}
+
+export default ContuctPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
